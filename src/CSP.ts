d1ea11eedec06ec8666bfb0884692aea75e7afd2
Exponential-Workload <cc@aaathats3as.com>
2023-12-11 20:35:52 +0100

    fix: did not mean to have that log

--- src/CSP.ts
+++ src/CSP.ts
@@ -17,10 +17,3 @@ const updateCSP = (csp: string, key: string, value: string): string => {
   return updatedCSP;
 };
 export default updateCSP;
-console.log(
-  updateCSP(
-    `connect-src 'self' checkout.stripe.com https://checkout.stripe.com https://billing.stripe.com/session https://api.funcaptcha.com https://api.arkoselabs.com sentry.io api.github.com www.npmjs.com;default-src 'none';img-src * data: https://*.stripe.com;script-src 'self' data: 'unsafe-inline' https://checkout.stripe.com/checkout.js https://checkout.stripe.com https://js.stripe.com/v3 https://platform.twitter.com/widgets.js https://octocaptcha.com https://static-production.npmjs.com/;style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://static-production.npmjs.com/;frame-src checkout.stripe.com https://checkout.stripe.com https://js.stripe.com/ https://octocaptcha.com;font-src https://fonts.gstatic.com https://static-production.npmjs.com/ ;media-src https://player.vimeo.com https://fpdl.vimeocdn.com https://gcs-vimeo.akamaized.net https://vod-progressive.akamaized.net`,
-    'yay-src',
-    'ts',
-  ),
-);
